541b7a433f46f95fd5ba2d538cf7247c1facb208
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-06-22 23:11:11 +0900

    [Feat] home, about page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,12 @@
+import styles from './footer.module.css';
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>KJS</div>
+      <div className={styles.text}>
+        KJS creative thoughts agency ⓒ All rights reserved.
+      </div>
+    </div>
+  );
 }
