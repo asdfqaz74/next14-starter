06f9596ccca4c1c79ac92db1b36a9d313df8c8f5
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-06-27 14:28:30 +0900

    [Feat] api/route.js 를 통한 api 통신 및 fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,24 +3,24 @@ import styles from './blog.module.css';
 import { getPosts } from '@/lib/data';
 
 // FETCH DATA WITH AN API
-// const getData = async () => {
-//   const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-//     next: { revalidate: 3600 },
-//   });
+const getData = async () => {
+  const response = await fetch('http://localhost:3000/api/blog', {
+    next: { revalidate: 3600 },
+  });
 
-//   if (!response.ok) {
-//     throw new Error('Something went wrong!');
-//   }
+  if (!response.ok) {
+    throw new Error('Something went wrong!');
+  }
 
-//   return response.json();
-// };
+  return response.json();
+};
 
 export default async function BlogPage() {
   // FETCH DATA WITH AN API
-  // const posts = await getData();
+  const posts = await getData();
 
   // FETCH DATA WITHOUT AN API
-  const posts = await getPosts();
+  // const posts = await getPosts();
 
   return (
     <div className={styles.container}>
